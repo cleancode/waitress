1df43bd4fc6bcb967696ef83b6b8b6fc68853a40
gabriele.lana <gabriele.lana@cleancode.it>
2013-11-11 17:52:40 +0100

    refactored helper for acceptance tests

--- api/test/acceptance/_helper.js
+++ api/test/acceptance/_helper.js
@@ -1,3 +1,21 @@
 process.env.NODE_ENV = "test"
 
-module.exports = {}
+var http = require("http"),
+    request = require("request")
+
+module.exports.startServer = function(app) {
+  return function(done) {
+    this.server = http.createServer(app).listen(app.get("port"), done)
+    this.route = function(route, expectation, done) {
+      request.get("http://localhost:" + app.get("port") + route, function(err, res, body) {
+        if (err) return done(err)
+        expectation(err, res, body)
+        done()
+      })
+    }
+  }
+}
+
+module.exports.stopServer = function(done) {
+  this.server.close(done)
+}
--- api/test/acceptance/hello.js
+++ api/test/acceptance/hello.js
@@ -1,44 +1,31 @@
 var helper = require("./_helper"),
     app = require("./../../app"),
-    expect = require("chai").expect,
-    http = require("http"),
-    request = require("request")
+    expect = require("chai").expect
 
 describe("Waitress", function() {
   describe("GET /hello", function() {
-    before(function(done) {
-      this.server = http.createServer(app).listen(app.get("port"), done)
-      request.get.route = function(route, expectation, done) {
-        request.get("http://localhost:" + app.get("port") + route, function(err, res, body) {
-          if (err) return done(err)
-          expectation(err, res, body)
-          done()
-        })
-      }
-    })
+    before(helper.startServer(app))
 
     it("should reply with 200 OK status", function(done) {
-      request.get.route("/hello", function(err, res, body) {
+      this.route("/hello", function(err, res, body) {
         expect(res.statusCode).to.equal(200)
       }, done)
     })
 
     it("should reply with 'Hello World' body", function(done) {
-      request.get.route("/hello", function(err, res, body) {
+      this.route("/hello", function(err, res, body) {
         expect(body).to.equal("Hello World")
       }, done)
     })
 
     describe("?who=Gabriele", function() {
       it("should reply with 'Hello Gabriele' body", function(done) {
-        request.get.route("/hello?who=Gabriele", function(err, res, body) {
+        this.route("/hello?who=Gabriele", function(err, res, body) {
           expect(body).to.equal("Hello Gabriele")
         }, done)
       })
     })
 
-    after(function(done) {
-      this.server.close(done)
-    })
+    after(helper.stopServer)
   })
 })
