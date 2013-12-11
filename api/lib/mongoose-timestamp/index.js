396709481b85dc8bdfcd1a0ae1ea513995e9e954
gabriele.lana <gabriele.lana@cleancode.it>
2013-12-11 11:09:16 +0100

    api: the default for a date field must be a closure that returns the current time otherwise the default time is when the process started, note that event the mongoose documentation has this bug

--- api/lib/mongoose-timestamp/index.js
+++ api/lib/mongoose-timestamp/index.js
@@ -1,7 +1,7 @@
 module.exports = function(schema, options) {
   schema.add({
-    createdAt: {type: Date, default: new Date()},
-    updatedAt: {type: Date, default: new Date()}
+    createdAt: {type: Date, default: function() {return new Date()}},
+    updatedAt: {type: Date, default: function() {return new Date()}}
   })
 
   schema.pre('save', function (next) {
