29c7f93ca445327bbf63afac59057cf7f1ae9dbe
gabriele.lana <gabriele.lana@cleancode.it>
2013-12-12 09:56:35 +0100

    api: ensure parseInt is always base 10... that brings me some funfuckingtastic moments

--- api/lib/connect-mongoose-sse/index.js
+++ api/lib/connect-mongoose-sse/index.js
@@ -7,7 +7,7 @@ module.exports = function(model) {
       return next()
     }
 
-    var startAt = parseInt(req.headers["last-event-id"] || "0"),
+    var startAt = parseInt(req.headers["last-event-id"] || "0", 10),
         endAt = (new Date()).getTime()
 
     model.createdBetween(startAt, endAt, function(err, docs) {
