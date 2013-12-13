896d1f0c6019a639b6003b2c33156b4d594ce0be
gabriele.lana <gabriele.lana@cleancode.it>
2013-12-13 12:04:51 +0100

    workshop: dishes

--- api/app.js
+++ api/app.js
@@ -3,6 +3,24 @@ var express = require('express'),
     mongoose = require('mongoose'),
     util = require('util')
 
+
+var Dish = mongoose.model('Dish', new mongoose.Schema(
+  {
+    name: String,
+    category: String
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      transform: function(doc, ret) {
+        delete ret._id
+        delete ret.__v
+      }
+    }
+  }
+))
+
+
 app.configure(function() {
   app.set('port', process.env.PORT || 3000)
   app.set('db', process.env.MONGODB_URL || 'mongodb://localhost/waitress')
@@ -13,7 +31,9 @@ app.configure('test', function() {
   app.set('db', 'mongodb://localhost/waitress-test')
 })
 
+app.use(express.favicon())
 app.use(express.logger('dev'))
+app.use(require('cors')())
 
 mongoose.connect(app.get('db'))
 
@@ -21,6 +41,12 @@ app.get('/hello', function(req, res) {
   res.end(
     util.format('Hello %s', req.query.who || 'World')
   )
+})
+
+app.get('/dishes', function(req, res) {
+  Dish.find().exec(function(err, docs) {
+    res.json(docs)
+  })
 })
 
 if (require.main === module) {
